3f11609eb4b6af332c752ce3f95bf56bed08e22b
yakovleva2904 <yakovlevaolga2904@gmail.com>
2022-01-25 16:17:04 +0200

    fixed 2d & 5th tasks

--- js/task-02.js
+++ js/task-02.js
@@ -7,12 +7,24 @@ const ingredients = [
   'Condiments',
 ];
 
-const list = document.querySelector('ul');
-console.log(list);
+// const list = document.querySelector('ul');
+// console.log(list);
 
-for (let i = 0; i < ingredients.length; i += 1) {
-  const listItem = document.createElement('li');
-  listItem.classList.add('item');
-  listItem.textContent = ingredients[i];
-  list.appendChild(listItem);
+// for (let i = 0; i < ingredients.length; i += 1) {
+//   const listItem = document.createElement('li');
+//   listItem.classList.add('item');
+//   listItem.textContent = ingredients[i];
+//   list.appendChild(listItem);
+// };
+
+const list = document.querySelector('ul');
+const ingredientList = (elements) => {
+  return elements.map(element => {
+    const listItem = document.createElement('li');
+    listItem.classList.add('item');
+    listItem.textContent = element;
+    return listItem;
+  });
 };
+const ingredientItems = ingredientList(ingredients);
+list.append(...ingredientItems);
--- js/task-05.js
+++ js/task-05.js
@@ -3,10 +3,17 @@ const spanEl = document.querySelector('span#name-output');
 console.log(inputEl);
 console.log(spanEl);
 
-inputEl.addEventListener("click", () => {
-    if (spanEl.textContent !== "") {
-        spanEl.textContent = inputEl.value;
-        return inputEl.value;
-    };
-    return spanEl.textContent = `Anonymous`;
+inputEl.addEventListener('input', (e) => {
+    spanEl.textContent = e.currentTarget.value;
+    if (e.currentTarget.value.length === 0) {
+        spanEl.textContent = 'Anonymous'
+    }
 });
+
+// inputEl.addEventListener("click", () => {
+//     if (spanEl.textContent !== "") {
+//         spanEl.textContent = inputEl.value;
+//         return inputEl.value;
+//     };
+//     return spanEl.textContent = `Anonymous`;
+// });
